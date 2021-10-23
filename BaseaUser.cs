0b708a93a0087f8c8d77e7a1d8773e637fad989b
Oleg <StavSonySony@gmail.com>
2021-10-23 19:51:55 +0300

    init

--- BaseaUser.cs
+++ BaseaUser.cs
@@ -12,7 +12,7 @@ namespace _21._10
             Email =emel;
         }
 
-         //public override string ToString() =>  $"Pasword - {Pasword ?? "is no data "} \nLogin - {Login ?? "is no data "} \nEmail - {Email} ";
+         public override string ToString() =>  $"Pasword - {Pasword ?? "is no data "} \nLogin - {Login ?? "is no data "} \nEmail - {Email} ";
         
         
 
